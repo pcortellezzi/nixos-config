001c9b6f61785f60bfbce6fbed19ad6c0e1d8d33
Philippe CORTELLEZZI <philippe.cortellezzi@gmail.com>
2025-10-11 05:29:32 -0300

    feat(nixos): Add udev rules to DisplayLink module

--- modules/services/displaylink.nix
+++ modules/services/displaylink.nix
@@ -7,6 +7,16 @@
   # 2. Active les règles udev de notre paquet (corrige la détection)
   services.udev.packages = [ pkgs.displaylink ];
 
+  # Add extra udev rules for stability
+  services.udev.extraRules = ''
+    # Disable USB3 Link Power Management for DisplayLink devices to improve stability
+    ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="17e9", ATTRS{bDeviceClass}!="09", RUN+="${pkgs.bash}/bin/bash -c 'echo 0 > /sys$devpath/../port/usb3_lpm_permit'"
+
+    # Configure CDC_NCM parameters for the DisplayLink ethernet adapter
+    ACTION=="add|move|bind", ATTRS{idVendor}=="17e9", ENV{ID_USB_DRIVER}=="cdc_ncm", ATTR{cdc_ncm/rx_max}="$attr{cdc_ncm/dwNtbInMaxSize}"
+    ACTION=="add|move|bind", ATTRS{idVendor}=="17e9", ENV{ID_USB_DRIVER}=="cdc_ncm", ATTR{cdc_ncm/tx_max}="$attr{cdc_ncm/dwNtbOutMaxSize}"
+  '';
+
   # 3. Définit et active le service systemd nous-mêmes (corrige le démarrage)
   systemd.services.displaylink = {
     description = "DisplayLink Manager Service";
