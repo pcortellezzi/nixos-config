ddd996d22ecb34fac2c663a8bba0b0d0989c5c97
Philippe CORTELLEZZI <philippe.cortellezzi@gmail.com>
2025-12-28 06:55:14 -0300

    feat(niri): config ouputs

--- modules/de/niri+dms.nix
+++ modules/de/niri+dms.nix
@@ -31,59 +31,6 @@ in
   programs.niri = {
     enable = true;
     useNautilus = true;
-
-    settings = {
-      outputs = {
-        "LG Electronics LG HDR WQHD 204NTHMB9585" = {
-          mode = {
-            width = 3440;
-            height = 1440;
-            refresh = 100.0;
-          };
-          position = {
-            x = 0;
-            y = 0;
-          };
-          scale = 1.0;
-        };
-       "LG Electronics LG HDR WQHD 204NTABB9600" = {
-          mode = {
-            width = 3440;
-            height = 1440;
-            refresh = 100.0;
-          };
-          position = {
-            x = 0;
-            y = 1440;
-          };
-          scale = 1.0;
-        };
-       "Iiyama North America PL2730Q 1219930721568" = {
-          mode = {
-            width = 2560;
-            height = 1440;
-            refresh = 60.0;
-          };
-          position = {
-            x = 3440;
-            y = 0;
-          };
-          scale = 1.0;
-        };
-       "eDP-1" = {
-          mode = {
-            width = 2880;
-            height = 1620;
-            refresh = 120.0;
-          };
-          position = {
-            x = 4880;
-            y = 1234;
-          };
-          scale = 1.75;
-        };
-      };
-    };
   };
 
   environment.systemPackages = with pkgs; [
