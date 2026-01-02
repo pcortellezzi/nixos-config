326f6fd5bb5ad3ac4d5b929f10b15c4cfe28197d
Philippe CORTELLEZZI <philippe.cortellezzi@gmail.com>
2026-01-01 22:06:39 -0300

    feat(greeter): switch from sddm to dms-greeter

--- modules/de/greeter.nix
+++ modules/de/greeter.nix
@@ -1,8 +1,16 @@
 { pkgs, ... }:
 
 {
-  services.displayManager.sddm = {
+  services.displayManager.dms-greeter = {
     enable = true;
-    wayland.enable = true;
+    compositor.name = "niri";  # Or "hyprland" or "sway"
+
+    # Sync your user's DankMaterialShell theme with the greeter. You'll probably want this
+    configHome = "/home/philippe";
+
+    # Custom config files for non-standard config locations
+    configFiles = [
+      "/home/philippe/.config/DankMaterialShell/settings.json"
+    ];
   };
 }
