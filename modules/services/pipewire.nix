f5a6ce44a093afcfda6bcec6a688bd05f478b25b
Philippe CORTELLEZZI <philippe.cortellezzi@gmail.com>
2026-01-30 04:54:25 -0300

    fix(pipewire): disable ALSA reservation in wireplumber to fix 100% CPU spikes

--- modules/services/pipewire.nix
+++ modules/services/pipewire.nix
@@ -6,5 +6,26 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    wireplumber = {
+      enable = true;
+      extraConfig = {
+        "10-disable-reserve" = {
+          "monitor.alsa.rules" = [
+            {
+              matches = [
+                {
+                  "device.name" = "~alsa_card.*";
+                }
+              ];
+              actions = {
+                update-props = {
+                  "api.alsa.reserve" = false;
+                };
+              };
+            }
+          ];
+        };
+      };
+    };
   };
 }
