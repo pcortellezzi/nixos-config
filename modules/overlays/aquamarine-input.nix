d429f06bb6ad7ce4f2847d7b2c7ffb88eaf390bb
Philippe CORTELLEZZI <philippe.cortellezzi@gmail.com>
2026-04-20 17:42:45 -0300

    fix: correct aquamarine overlay format
    Overlay should be a function, not an attrset with nixpkgs.overlays.

--- modules/overlays/aquamarine-input.nix
+++ modules/overlays/aquamarine-input.nix
@@ -1,11 +1,8 @@
 # Overlay to patch aquamarine input for EVDI/DisplayLink compatibility
 { inputs, ... }:
-{
-  nixpkgs.overlays = [
-    (final: prev: {
-      aquamarine = inputs.aquamarine.packages.${prev.stdenv.hostPlatform.system}.aquamarine.overrideAttrs (old: {
-        patches = (old.patches or []) ++ [ ./aquamarine-evdi.patch ];
-      });
-    })
-  ];
+
+final: prev: {
+  aquamarine = inputs.aquamarine.packages.${prev.stdenv.hostPlatform.system}.aquamarine.overrideAttrs (old: {
+    patches = (old.patches or []) ++ [ ./aquamarine-evdi.patch ];
+  });
 }
