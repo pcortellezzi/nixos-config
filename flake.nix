2db2a870387794bc0af578e90de70336a0c6671f
Philippe CORTELLEZZI <philippe.cortellezzi@gmail.com>
2026-04-22 16:35:34 -0300

    feat(display): update eDP-1 resolution to 1920x1080 and scale to 1.0

--- flake.nix
+++ flake.nix
@@ -28,6 +28,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    agent-convergence = {
+      url = "github:pcortellezzi/agent-convergence";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     dms = {
       url = "github:AvengeMedia/DankMaterialShell";
       inputs.nixpkgs.follows = "nixpkgs";
