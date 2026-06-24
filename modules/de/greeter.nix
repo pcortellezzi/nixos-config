dce898913e25c124c8b1c23fd9beddb5d904e8ef
Philippe CORTELLEZZI <philippe.cortellezzi@gmail.com>
2026-06-24 16:34:41 -0300

    fix sddm vkbd: add maliit-keyboard inputmethod backend for kwin

--- modules/de/greeter.nix
+++ modules/de/greeter.nix
@@ -1,9 +1,23 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
-{
+let
+  maliit-keyboard = "${pkgs.maliit-keyboard}/bin/maliit-keyboard";
+  kwin = lib.concatStringsSep " " [
+    "${lib.getBin pkgs.kdePackages.kwin}/bin/kwin_wayland"
+    "--no-global-shortcuts"
+    "--no-kactivities"
+    "--no-lockscreen"
+    "--locale1"
+    "--inputmethod ${maliit-keyboard}"
+  ];
+in {
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
-  services.displayManager.sddm.extraPackages = [ pkgs.kdePackages.plasma-workspace ];
+  services.displayManager.sddm.extraPackages = [
+    pkgs.kdePackages.plasma-workspace
+    pkgs.maliit-keyboard
+  ];
+  services.displayManager.sddm.settings.Wayland.CompositorCommand = kwin;
 
   systemd.services.display-manager.environment.KWIN_IM_SHOW_ALWAYS = "1";
 }
