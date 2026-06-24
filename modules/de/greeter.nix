4a82e07ae1eb52ac1dfec74b23711e98237b78a9
Philippe CORTELLEZZI <philippe.cortellezzi@gmail.com>
2026-06-24 14:05:50 -0300

    sddm: enable virtual keyboard (qtvirtualkeyboard)
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- modules/de/greeter.nix
+++ modules/de/greeter.nix
@@ -3,4 +3,8 @@
 {
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm.virtualKeyboard = {
+    enable = true;
+    layout = "fr";
+  };
 }
