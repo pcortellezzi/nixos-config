5a994d712b25de4d221952f75fbc280ff4755acc
Philippe CORTELLEZZI <philippe.cortellezzi@gmail.com>
2026-06-24 16:28:18 -0300

    fix sddm vkbd: add KWIN_IM_SHOW_ALWAYS=1 env var so kwin shows keyboard on click

--- modules/de/greeter.nix
+++ modules/de/greeter.nix
@@ -4,4 +4,6 @@
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
   services.displayManager.sddm.extraPackages = [ pkgs.kdePackages.plasma-workspace ];
+
+  systemd.services.display-manager.environment.KWIN_IM_SHOW_ALWAYS = "1";
 }
