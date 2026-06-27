338036247ede09a9d90f9afca3aee7785b4bcaa2
Philippe CORTELLEZZI <philippe.cortellezzi@gmail.com>
2026-06-27 16:28:43 -0300

    Revert "greeter: enable X11 session alongside Wayland for Plasma 6"
    This reverts commit c4cc6f3197db27a10075ff6b9250d646c7767ad1.

--- modules/de/greeter.nix
+++ modules/de/greeter.nix
@@ -11,8 +11,6 @@ let
     "--inputmethod ${maliit-keyboard}"
   ];
 in {
-  services.xserver.enable = true;
-
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
   services.displayManager.sddm.extraPackages = [
